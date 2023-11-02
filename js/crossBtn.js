8fdb46d1d8b375c4fdb96ab7afab047b083e3fe9
mirza-azam <mirzaazam884@gmail.com>
2023-11-02 23:42:56 +0600

    added cross btn

---
+++ js/crossBtn.js
@@ -0,0 +1,9 @@
+document.getElementById('phone-remove').addEventListener('click', function (){
+    const phoneRow = document.getElementById('phone-row');
+    phoneRow.style.display = 'none';
+});
+
+document.getElementById('row-case').addEventListener('click', function(){
+    const rowCase = document.getElementById('row-case');
+    rowCase.style.display = 'none';
+});
